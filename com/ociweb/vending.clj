a79d519c912c303af383c231fb0d2309d05722d6
R. Mark Volkmann <mark@ociweb.com>
2009-04-29 19:52:42 -0500

    many small improvements

--- com/ociweb/vending.clj
+++ com/ociweb/vending.clj
@@ -6,8 +6,6 @@
 (ns com.ociweb.vending
   (:use [clojure.contrib.seq-utils :only (find-first)]))
 
-;---------------------------------------------------------------------------
-
 (defstruct item-struct :description :price :quantity)
 
 ;---------------------------------------------------------------------------
@@ -24,55 +22,14 @@
 
 ;---------------------------------------------------------------------------
 
-(defn currency
-  "returns a currency formatted string for a given dollar amount"
-  [dollars]
-  (let [formatter (java.text.NumberFormat/getCurrencyInstance)]
-    (.format formatter dollars)))
+(defn reset-machine
+  "resets the state of the vending machine"
+  []
+  (dosync
+    (ref-set amount-inserted-ref 0)
+    (ref-set item-map-ref (sorted-map))
+    (ref-set money-map-ref (sorted-map))))
 
-(def money-code
-  {0.05 \n, 0.10 \d, 0.25 \q, 1.0 \1})
-    
-(def money-name
-  {0.05 "nickel", 0.10 "dime", 0.25 "quarter", 1.0 "dollar"})
-    
-(defn money-string [value quantity]
-  (str quantity " " (money-name value) (when (> quantity 1) "s")))
-
-(defn remove-coin
-  "returns a new map with th egiven coin removed"
-  [money-map value]
-  (let [quantity (money-map value)]
-    (condp = quantity
-      nil (throw (RuntimeException.
-                   (str "can't remove coin with value " value)))
-      1 (dissoc money-map value)
-      (assoc money-map value (dec quantity)))))
-
-(defn nearly-equal [v1 v2]
-  (< (Math/abs (- v1 v2)) 1e-7))
-
-(declare make-change)
-
-(defn make-change-internal
-  "returns true if successful and false if it couldn't make correct change."
-  [amount money-map change value]
-  (cond
-    (nearly-equal value amount) (conj change value)
-    (< value amount)
-    (make-change
-      (- amount value)
-      (remove-coin money-map value)
-      (conj change value))
-    true nil))
-
-(defn make-change
-  ([amount money-map] (make-change amount money-map []))
-  ([amount money-map change]
-     (some ; returns the first non-nil result
-       #(make-change-internal amount money-map change %)
-       (reverse (keys money-map)))))
-  
 (defn add-item
   "adds an item to the vending machine"
   [selector description price quantity]
@@ -84,13 +41,24 @@
   [value quantity]
   (dosync (alter money-map-ref assoc value quantity)))
 
-(defn show-change
-  "shows the change the vending machine currently holds"
-  []
-  (println "machine holds:")
-  (doseq [[value quantity] @money-map-ref]
-    (println (money-string value quantity))))
-  
+(defn fill-machine []
+  (reset-machine)
+  (add-item "A" "Juicy Fruit" 0.65 3)
+  (add-item "B" "Baked Lays" 1.00 2)
+  (add-item "C" "Pepsi" 1.50 4)
+  (add-money 0.05 5)
+  (add-money 0.10 3)
+  (add-money 0.25 4)
+  (add-money 1.00 2))
+
+;---------------------------------------------------------------------------
+
+(defn currency
+  "returns a currency formatted string for a given dollar amount"
+  [dollars]
+  (let [formatter (java.text.NumberFormat/getCurrencyInstance)]
+    (.format formatter dollars)))
+
 (defn help
   "outputs help instructions"
   []
@@ -107,16 +75,32 @@
   1 - enter a dollar bill
   uppercase letter - buy item with that selector"))
 
-(defn coin-return
-  "returns the unused money that has been inserted"
-  []
-  (when-let [change (make-change @amount-inserted-ref @money-map-ref)]
-    (dosync
-      (doseq [value change]
-        (println (money-code value))
-        (ref-set money-map-ref (remove-coin @money-map-ref value)))
-      (ref-set amount-inserted-ref 0))))
+(defn insert-money
+  "inserts money in the vending machine for purchasing an item"
+  [value]
+  (dosync
+    ; 0 is the default value returned by get
+    ; when the money map doesn't contain a key equal to value.
+    (let [new-quantity (inc (get @money-map-ref value 0))]
+      (alter money-map-ref assoc value new-quantity)
+      (alter amount-inserted-ref + value))))
 
+(def money-code
+  {0.05 \n, 0.10 \d, 0.25 \q, 1.0 \1})
+    
+(def money-name
+  {0.05 "nickel", 0.10 "dime", 0.25 "quarter", 1.0 "dollar"})
+    
+(defn money-string [value quantity]
+  (str quantity " " (money-name value) (when (> quantity 1) "s")))
+
+(defn show-change
+  "shows the change the vending machine currently holds"
+  []
+  (println "machine holds:")
+  (doseq [[value quantity] @money-map-ref]
+    (println (money-string value quantity))))
+  
 (defn show-inserted
   "shows the amount of money that has been inserted and not yet spent"
   []
@@ -125,22 +109,68 @@
 (defn show-items
   "shows the items the vending machine currently holds"
   []
-  (doseq [selector (keys @item-map-ref)]
-    (let [item (@item-map-ref selector)]
-      (println selector "-"
-        (item :quantity)
-        (item :description)
-        (currency (item :price))))))
+  (doseq [[selector, item] @item-map-ref]
+    (println selector "-"
+      (item :quantity)
+      (item :description)
+      (currency (item :price)))))
   
-(defn insert-money
-  "inserts money in the vending machine for purchasing an item"
-  [value]
-  (dosync
-    (let [new-quantity (inc (get @money-map-ref value 0))]
-      (alter money-map-ref assoc value new-quantity)
-      (alter amount-inserted-ref + value))))
+;---------------------------------------------------------------------------
+ 
+(defn nearly-equal [v1 v2]
+  (< (Math/abs (- v1 v2)) 1e-7))
 
-(defn- purchase [selector item]
+(defn remove-coin
+  "returns a new map with th egiven coin removed"
+  [money-map value]
+  (let [quantity (money-map value)]
+    (condp = quantity
+      nil (throw (RuntimeException.
+                   (str "can't remove coin with value " value)))
+      1 (dissoc money-map value)
+      (assoc money-map value (dec quantity)))))
+
+(declare make-change)
+
+(defn make-change-internal
+  "This attempts to make an amount of change
+   using a given coin value
+   and the coins in money-map.
+   If successful, the coins needed to make the change
+   are added to the change vector and that is returned.
+   Otherwise nil is returned."
+  [amount value money-map change]
+  (cond
+    (nearly-equal value amount) (conj change value) ; success
+    (< value amount) ; subtract value and try to make remaining change
+      (make-change
+        (- amount value)
+        (remove-coin money-map value)
+        (conj change value))
+    true nil)) ; failure
+
+(defn make-change
+  ([amount money-map]
+    (make-change amount money-map []))
+  ([amount money-map change]
+     (some ; stop on first non-nil result and return it
+       #(make-change-internal amount % money-map change)
+       ; try highest value coins first
+       (reverse (keys money-map)))))
+  
+(defn coin-return
+  "ejects the unused money that has been inserted"
+  []
+  (when-let [change (make-change @amount-inserted-ref @money-map-ref)]
+    (dosync
+      (doseq [value change]
+        (println (money-code value))
+        (ref-set money-map-ref (remove-coin @money-map-ref value)))
+      (ref-set amount-inserted-ref 0))))
+
+;---------------------------------------------------------------------------
+
+(defn purchase [selector item]
   (let [new-quantity (dec (item :quantity))
         new-item (assoc item :quantity new-quantity)]
     (dosync
@@ -149,7 +179,7 @@
   (println selector)
   (coin-return))
 
-(defn- attempt-purchase [selector item]
+(defn attempt-purchase [selector item]
   (if (zero? (item :quantity))
     (println "sold out")
     (let [price (item :price)
@@ -165,6 +195,8 @@
     (attempt-purchase selector item)
     (println "invalid command or selector" (str "\"" selector "\""))))
     
+;---------------------------------------------------------------------------
+ 
 ; TODO: Consider changing this to call functions with eval
 ; TODO: to be more like the Ruby version.
 (defn command [cmd]
@@ -183,57 +215,15 @@
     "1" (insert-money 1.00)
     (select cmd)))
 
-;---------------------------------------------------------------------------
-; Code above this has been compared to Ruby version.
-;---------------------------------------------------------------------------
-
-(defn- not-more-and-available
-  "determines if a given coin denomination is both
-   not more than inserted and available in the machine"
-  [denomination inserted]
-  (and (<= denomination inserted) (@money-map-ref denomination)))
-
-(defn quantity
-  "gets the quantity remaining for a given item selector"
-  [selector]
-  (let [item (@item-map-ref selector)]
-    (item :quantity)))
-
-(defn reset-machine
-  "resets the state of the vending machine"
+(defn main
+  "called by vending_main.clj"
   []
-  (dosync
-    (ref-set amount-inserted-ref 0)
-    (ref-set item-map-ref (sorted-map))
-    (ref-set money-map-ref (sorted-map))))
+  (fill-machine)
 
-(defn fill-machine []
-  (reset-machine)
-  (add-item "A" "Juicy Fruit" 0.65 3)
-  (add-item "B" "Baked Lays" 1.00 2)
-  (add-item "C" "Pepsi" 1.50 4)
-  (add-money 0.05 5)
-  (add-money 0.10 3)
-  (add-money 0.25 4)
-  (add-money 1.00 2))
-
-; Note that this is called by vending_main.clj.
-(defn main []
-  ; Add items to vending machine.
-  (add-item "A" "Juicy Fruit" 0.65 3)
-  (add-item "B" "Baked Lays" 1.00 2)
-  (add-item "C" "Pepsi" 1.50 4)
-
-  ; Add money to vending machine.
-  (add-money 0.05 5)
-  (add-money 0.10 3)
-  (add-money 0.25 4)
-  (add-money 1.00 2)
-
-  ; Process input.
   (println "Enter commands such as \"help\".")
-  (loop []
+  (loop [] ; process input
     (print "> ") (flush)
     (let [cmd (read-line)]
       (command cmd)
-      (when-not (or (= cmd "exit") (= cmd "quit")) (recur)))))
+      (when-not (or (= cmd "exit") (= cmd "quit"))
+        (recur)))))
